3ef55910f84253b49385ea297e5e43205b257402
Tim <tim.maastricht@gmail.com>
2026-02-04 19:28:13 +1000

    feat: added pokemon package

---
+++ src/getPokemonName.js
@@ -0,0 +1,11 @@
+async function getPokemonName() {
+  let targetId = Math.floor(Math.random() * 1025) + 1;
+  let response = await fetch("https://pokeapi.co/api/v2/pokemon/" + targetId);
+  let data = await response.json();
+
+  return data.name;
+}
+
+module.exports = {
+  getPokemonName,
+};
--- src/main.js
+++ src/main.js
@@ -1,1 +1,21 @@
-console.log("Hello, world!")
+// const { getPokemonName } = require("./getPokemonName");
+// method 1 for importing and using stuff from packages
+//const pokemon = require("pokemon"); // object that has a property that can access data
+//console.log(pokemon.random())
+
+// method 2 for importing and using stuff from packages
+// object destructuring
+const {random} = require("pokemon");
+console.log(random()) // generic named
+const {random: alias} = require("pokemon");
+console.log(alias()) // alias
+
+
+// // Import a function and run it
+// async function app() {
+//   let pokemonName = await getPokemonName();
+//   //console.log(getPokemonName);
+//   console.log(await getPokemonName());
+//   console.log(pokemonName);
+// }
+// app();
